93cd426b1bf4407ccb8243a5259cabdf2e8fd362
xiadw <xiadw_public@163.com>
2019-12-01 12:02:10 +0800

    finish get questions endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -50,6 +50,22 @@ def create_app(test_config=None):
   ten questions per page and pagination at the bottom of the screen for three pages.
   Clicking on the page numbers should update the questions. 
   '''
+  @app.route('/questions', methods=['GET'])
+  def get_questions():
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * 10
+    end = start + 10
+    questions = Question.query.all()
+    categories = Category.query.all()
+    formated_questions = [question.format() for question in questions]
+    formated_categories = [category.format() for category in categories]
+
+    return jsonify({
+      'questions': formated_questions[start:end],
+      'total_questions': len(formated_questions),
+      'categories': formated_categories,
+      'current_category': None
+    })
 
   '''
   @TODO: 
